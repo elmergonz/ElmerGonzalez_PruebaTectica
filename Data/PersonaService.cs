475d4c03cc65fb89387459ea9b0b8f789a78a729
Elmer <elmer.go@outlook.com>
2021-07-29 11:47:18 -0400

    avance

--- Data/PersonaService.cs
+++ Data/PersonaService.cs
@@ -11,5 +11,45 @@ namespace ElmerGonzalez_PruebaTectica.Data
         {
             return Task.FromResult(new bcwi0rmh1y8kmv36xayuContext().Personas.ToArray());
         }
+
+        public void PostPersona(Persona persona)
+        {
+            bcwi0rmh1y8kmv36xayuContext context = new();
+            context.Personas.Add(persona);
+            context.SaveChanges();
+        }
+
+        public bool PutPersona(Persona persona)
+        {
+            bcwi0rmh1y8kmv36xayuContext context = new();
+            Persona oldPersona = context.Personas.First(p => p.Id == persona.Id);
+            
+            if (oldPersona != null)
+            {
+                oldPersona.Nombre = persona.Nombre;
+                oldPersona.FechaNacimiento = persona.FechaNacimiento;
+                context.SaveChanges();
+                
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool DeletePersona(int Id)
+        {
+            bcwi0rmh1y8kmv36xayuContext context = new();
+            Persona persona = context.Personas.First(p => p.Id == Id);
+
+            if (persona != null)
+            {
+                context.Personas.Remove(persona);
+                context.SaveChanges();
+                
+                return true;
+            }
+
+            return false;
+        }
     }
 }
